83f2905df4763c5d6e7e107d4a467cfd49e0f190
Valentin <primarh111@mail.ru>
2020-08-17 15:54:56 +0300

    header + slider

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.shares__slider').slick({
+        dots: true,
+        prevArrow: '<button class="shares__slider-btn shares__slider-btnprev"><img src="img/icons/left__arrow.svg" alt=""></button>',
+        nextArrow: '<button class="shares__slider-btn shares__slider-btnnext"><img src="img/icons/right__arrow.svg" alt=""></button>',
+    })
+});
