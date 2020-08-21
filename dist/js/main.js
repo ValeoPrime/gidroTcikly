7a5994d3987abd9c249ccd2b912ff9d52bd4035e
Valentin <primarh111@mail.ru>
2020-08-21 10:00:36 +0300

    main cont+filter part 1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.shares__slider').slick({
         dots: true,
         prevArrow: '<button class="shares__slider-btn shares__slider-btnprev"><img src="img/icons/left__arrow.svg" alt=""></button>',
@@ -13,6 +13,19 @@ $(function(){
         prevArrow: '<button class="popular__slider-btn popular__slider-btnprev"><img src="img/icons/left__arrow-black.svg" alt=""></button>',
         nextArrow: '<button class="popular__slider-btn popular__slider-btnnext"><img src="img/icons/right__arrow-black.svg" alt=""></button>',
     });
+
+    $('.filter__style').styler()
+
+    //Дропдауны в фильтре
+    $('.title-drop').on('click', function(){
+        $(this).toggleClass('title-drop-active');
+        $(this).next().slideToggle(500)
+    })
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        
+    });
 });
 
 // ТАБЫ
@@ -20,36 +33,45 @@ const searchTab = document.querySelector('.search__tabs')
 const tabs = document.querySelectorAll('.search__tab_item')
 const searchContents = document.querySelectorAll('.search__content-item')
 
-searchTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs (tabs, searchContents, 'active_tab', 'active__search__content-item')
-})
+// searchTab.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     changeTabs (tabs, searchContents, 'active_tab', 'active__search__content-item')
+// })
 
 const popularTab = document.querySelector('.popular__categories')
 const populartabs = document.querySelectorAll('.popular__categories_item')
 const popularContents = document.querySelectorAll('.popular__slider')
 
-popularTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs (populartabs, popularContents, 'active__tab', 'popular__slider--active')
-})
+// popularTab.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     changeTabs (populartabs, popularContents, 'active__tab', 'popular__slider--active')
+// })
 
 const prodTab = document.querySelector('.prod__categories')
 const prodtabs = document.querySelectorAll('.prod__categories_item')
 const prodContents = document.querySelectorAll('.prod__slider')
 
-prodTab.addEventListener('click', (event) => {
+// prodTab.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     changeTabs (prodtabs, prodContents, 'active__tab', 'popular__slider--active')
+// })
+
+const filterTab = document.querySelector('.filter__tabs')
+const filtertabs = document.querySelectorAll('.filter__tabs-link')
+const filterContents = document.querySelectorAll('.filter__tabs_content')
+
+filterTab.addEventListener('click', (event) => {
     event.preventDefault()
-    changeTabs (prodtabs, prodContents, 'active__tab', 'popular__slider--active')
+    changeTabs(filtertabs, filterContents, 'active__filter_tab', 'active__filter_content')
 })
 
-function changeTabs (tabsArr, contentArr, activeTabClass, activeContClass) {
+function changeTabs(tabsArr, contentArr, activeTabClass, activeContClass) {
     tabsArr.forEach(item => {
         item.classList.remove(activeTabClass)
     })
     contentArr.forEach(i => {
         i.classList.remove(activeContClass)
-        if(event.target.getAttribute('href').slice(1, 20) == i.id) {
+        if (event.target.getAttribute('href').slice(1, 20) == i.id) {
             i.classList.add(activeContClass)
         }
     })
@@ -63,8 +85,9 @@ const cards = document.querySelectorAll('.popular__product_item')
 cards.forEach(item => {
     item.addEventListener('click', (event) => {
         event.preventDefault()
-        if(event.target.classList.contains('popular__product_item-favorite')){
+        if (event.target.classList.contains('popular__product_item-favorite')) {
             event.target.classList.toggle('popular__product_item-favorite--active')
         }
     })
 })
+
