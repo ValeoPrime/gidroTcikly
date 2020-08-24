fd2e70354751b7d9608f1b205b9166e8d6a340e6
Valentin <primarh111@mail.ru>
2020-08-24 19:55:39 +0300

    descktop all pages

--- src/js/main.js
+++ src/js/main.js
@@ -13,9 +13,9 @@ $(function () {
         prevArrow: '<button class="popular__slider-btn popular__slider-btnprev"><img src="img/icons/left__arrow-black.svg" alt=""></button>',
         nextArrow: '<button class="popular__slider-btn popular__slider-btnnext"><img src="img/icons/right__arrow-black.svg" alt=""></button>',
     });
-
+    //Стилизация чекбоксов. Селектов. Радио
     $('.filter__style').styler()
-
+    //Переключение отображения карточек в каталоге
     $('.catalog__filters-btngrid').on('click', function () {
         $(this).addClass('catalog__filters-btn-active');
         $('.catalog__filters-btnline').removeClass('catalog__filters-btn-active');
@@ -29,7 +29,7 @@ $(function () {
     })
 
     //Дропдауны в фильтре
-    $('.title-drop').on('click', function(){
+    $('.title-drop').on('click', function () {
         $(this).toggleClass('title-drop-active');
         $(this).next().slideToggle(500)
     })
@@ -37,6 +37,16 @@ $(function () {
     $(".js-range-slider").ionRangeSlider({
         type: "double",
     });
+
+
+    $(".rate-Yo").rateYo({
+        normalFill: "#c4c4c4",
+        ratedFill: "#1C62CD",
+        spacing: "7px",
+        starWidth: "23px"
+    });
+
+
 });
 
 // ТАБЫ
@@ -44,48 +54,86 @@ const searchTab = document.querySelector('.search__tabs')
 const tabs = document.querySelectorAll('.search__tab_item')
 const searchContents = document.querySelectorAll('.search__content-item')
 
-searchTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs (tabs, searchContents, 'active_tab', 'active__search__content-item')
-})
+if (searchTab !== null) {
+    searchTab.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(tabs, searchContents, 'active_tab', 'active__search__content-item')
+    })
+}
+
 
 const popularTab = document.querySelector('.popular__categories')
 const populartabs = document.querySelectorAll('.popular__categories_item')
 const popularContents = document.querySelectorAll('.popular__slider')
 
-popularTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs (populartabs, popularContents, 'active__tab', 'popular__slider--active')
-})
+if (popularTab !== null) {
+    popularTab.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(populartabs, popularContents, 'active__tab', 'popular__slider--active')
+    })
+}
+
 
 const prodTab = document.querySelector('.prod__categories')
 const prodtabs = document.querySelectorAll('.prod__categories_item')
 const prodContents = document.querySelectorAll('.prod__slider')
 
-prodTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs (prodtabs, prodContents, 'active__tab', 'popular__slider--active')
-})
+if (prodTab !== null) {
+    prodTab.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(prodtabs, prodContents, 'active__tab', 'popular__slider--active')
+    })
+}
+
 
 const filterTab = document.querySelector('.filter__tabs')
 const filtertabs = document.querySelectorAll('.filter__tabs-link')
 const filterContents = document.querySelectorAll('.filter__tabs_content')
 
-filterTab.addEventListener('click', (event) => {
-    event.preventDefault()
-    changeTabs(filtertabs, filterContents, 'active__filter_tab', 'active__filter_content')
-})
+if (filterTab !== null) {
+    filterTab.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(filtertabs, filterContents, 'active__filter_tab', 'active__filter_content')
+    })
+}
+
+const productCard = document.querySelector('.productCard__tabs')
+const productCardtabs = document.querySelectorAll('.productCard__tab_item')
+const productCardСontent = document.querySelectorAll('.productCard__content-item')
+
+if (productCard !== null) {
+    productCard.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(productCardtabs, productCardСontent, 'active_tab', 'active__productCard__content-item')
+    })
+}
+
+const productCardT = document.querySelector('.productCardTabs__tabs')
+const productCardTabsitems = document.querySelectorAll('.productCardTabs_item')
+const productCardTabsContent = document.querySelectorAll('.productCardTabs__content-item')
+
+if (productCardT !== null) {
+    productCardT.addEventListener('click', (event) => {
+        event.preventDefault()
+        changeTabs(productCardTabsitems, productCardTabsContent, 'active_tab', 'active__productCard__content-item')
+    })
+}
 
 function changeTabs(tabsArr, contentArr, activeTabClass, activeContClass) {
-    tabsArr.forEach(item => {
-        item.classList.remove(activeTabClass)
-    })
-    contentArr.forEach(i => {
-        i.classList.remove(activeContClass)
-        if (event.target.getAttribute('href').slice(1, 20) == i.id) {
-            i.classList.add(activeContClass)
-        }
-    })
+    if(event.target.getAttribute('href') !== null){
+        tabsArr.forEach(item => {
+            item.classList.remove(activeTabClass)
+        })
+        
+        contentArr.forEach(i => {
+            i.classList.remove(activeContClass)
+            
+                if (event.target.getAttribute('href').slice(1, 20) == i.id) {
+                    i.classList.add(activeContClass)
+                }
+        
+        })
+    }
 
     event.target.classList.add(activeTabClass)
 }
@@ -101,4 +149,19 @@ cards.forEach(item => {
         }
     })
 })
+
+
+
+const body = document.querySelector('body')
+const popup = document.querySelector('.popUp__info')
+if(popup !== null){
+    setTimeout(() => {
+        popup.classList.add('show')
+        setTimeout(() => {
+            popup.classList.remove('show')
+        }, 8000);
+    }, 3000);
+}
+
+
 
